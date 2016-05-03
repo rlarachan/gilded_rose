fa79a4a522e7156d4ede75884b6f7edda7a3f09c
Lara Verenice Martinez <lvrmk03@gmail.com>
2016-05-03 21:34:02 +0800

    modifies gilded rose class

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -12,39 +12,51 @@ class GildedRose
       case item.name
       when "Aged Brie"
         if item.sell_in >= 0
-          item.quality += 1
+          quality_up(item, 1)
         else
-          item.quality += 2
+          quality_up(item, 2)
         end
+
       when "Backstage passes to a TAFKAL80ETC concert"
         case
         when item.sell_in > 10
-          item.quality += 1
+          quality_up(item, 1)
         when item.sell_in.between?(6, 10)
-          item.quality += 2
+          quality_up(item, 2)
         when item.sell_in.between?(0, 5)
-          item.quality += 3
+          quality_up(item, 3)
         else
           item.quality = 0
         end
 
       when "Conjured"
         if item.sell_in >= 0
-          item.quality -= 2
+          quality_down(item, 2)
         else
-          item.quality -= 4
+          quality_down(item, 4)
         end
       else
         if item.sell_in >= 0
-          item.quality -= 1
+          quality_down(item, 1)
         else
-          item.quality -= 2
+          quality_down(item, 2)
         end
       end
 
     end
   end
 
+private
+
+  def quality_up(item, value)
+    sum = item.quality + value
+    item.quality = sum > 50 ? 50 : sum
+  end
+
+  def quality_down(item, value)
+    diff = item.quality - value
+    item.quality = diff < 0 ? 0 : diff
+  end
 end
 
 class Item
