2ce58e1f3b065cbf835e9601703c423df3ec3ccb
Lara Verenice Martinez <lvrmk03@gmail.com>
2016-05-03 17:20:25 +0800

    refactors #update_quality for aged brie, backstage passes & conjured

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,14 +6,42 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in -= 1
+      next if item.name == "Sulfuras, Hand of Ragnaros"
+      item.sell_in -= 1
+
+      case item.name
+      when "Aged Brie"
+        if item.sell_in >= 0
+          item.quality += 1
+        else
+          item.quality += 2
+        end
+      when "Backstage passes to a TAFKAL80ETC concert"
+        case
+        when item.sell_in > 10
+          item.quality += 1
+        when item.sell_in.between?(6, 10)
+          item.quality += 2
+        when item.sell_in.between?(0, 5)
+          item.quality += 3
+        else
+          item.quality = 0
+        end
+
+      when "Conjured"
+        if item.sell_in >= 0
+          item.quality -= 2
+        else
+          item.quality -= 4
+        end
+      else
         if item.sell_in >= 0
           item.quality -= 1
         else
           item.quality -= 2
         end
       end
+
     end
   end
 
